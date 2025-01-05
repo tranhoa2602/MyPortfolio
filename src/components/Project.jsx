a268edf35c300054d2ddd821071f3c2ce67f927f
tranhoa2602 <thdblacksky@gmail.com>
2025-01-05 16:04:26 +0700

    update

---
+++ src/components/Project.jsx
@@ -0,0 +1,83 @@
+import { PROJECTS } from "../constants";
+import { motion } from "framer-motion";
+
+const Project = () => {
+  return (
+    <div className="border-b border-neutral-900 pl-4">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h2>
+      <div>
+        {PROJECTS.map((project, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1.5 }}
+              className="w-full lg:w-1/4"
+            >
+              <img
+                src={project.image}
+                width={250}
+                height={250}
+                alt={project.title}
+                className="mb-6 rounded"
+              />
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1.5 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
+              <h6 className="mb-2 font-semibold">
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:underline"
+                >
+                  {project.title}
+                </a>
+              </h6>
+              <p className="mb-4 text-neutral-400">{project.description}</p>
+              <div className="mb-4">
+                {project.technologies.map((tech, techIndex) => (
+                  <span
+                    key={techIndex}
+                    className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              {project.video && (
+                <div className="mb-4">
+                  <video controls width="50%" className="rounded">
+                    <source src={project.video} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                </div>
+              )}
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+              >
+                View Details
+              </a>
+            </motion.div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Project;
---
+++ src/constants/index.js
@@ -0,0 +1,72 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+import game1 from "../assets/projects/game-1.mp4";
+import game2 from "../assets/projects/game-2.mp4";
+
+export const HERO_CONTENT = `I am a dedicated character animator with a passion for bringing life and personality to game characters. As a recent graduate, I have developed skills in 2D and 3D animation, specializing in character rigging, movement design. My experience extends to implementing animations in Unreal Engine 5, ensuring smooth and dynamic integration into gameplay.`;
+
+export const ABOUT_TEXT = `I recently completed my mobile development internship, but my true passion lies in game development, especially character animation in Unreal Engine 5. Driven by curiosity, I’ve spent countless hours exploring how games are crafted, learning the tools, and practicing techniques. My focus is on bringing characters to life with realistic and dynamic movements using Animation Blueprints and State Machines. I’m excited to contribute to creative gaming projects and continue growing in this ever-evolving field`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Animation Game Developer",
+    company: "Work At Home",
+    description: `I focus on creating realistic and dynamic movements through Animation Blueprints and State Machines.`,
+    technologies: ["Unreal Engine", "Blueprints", "State Machine"],
+  },
+  {
+    year: "2023 - 2024",
+    role: "Frontend Developer",
+    company: "Amit Group JSC",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "FireBase"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "SuperHot Game",
+    image: project3,
+    description:
+      "I am developing a basic first-person shooter game to master the implementation of shooting mechanics and character animations.",
+    technologies: ["Unreal Engine", "Blueprints", "State Machine"],
+    link: "https://drive.google.com/file/d/19wzXuCPYw9K6CNa4h17-sn2KxN6HrtOw/view?usp=sharing",
+    video: game1,
+  },
+  {
+    title: "Game 2D",
+    image: project1,
+    description:
+      "This is a 2D game about a knight on her adventurous journey. Along the way, the female knight encounters numerous monsters and traps. She must overcome all obstacles to continue her journey.",
+    technologies: ["Unreal Engine", "Blueprints", "State Machine"],
+    link: "https://drive.google.com/file/d/1XQbQ0bn-1tuqSGSkHdpLw_or3DuA1b4X/view?usp=sharing",
+    video: game2,
+  },
+  {
+    title: "Game Zombie Survival",
+    image: project2,
+    description:
+      "This is a game about a man who must survive in a forest full of zombies. Within a certain time limit, the man must kill all the zombies, or the game will end.",
+    technologies: ["Unreal Engine", "Blueprints", "State Machine"],
+    link: "https://drive.google.com/file/d/15FAAmjdR6u2lLtKtVyGsyaZ6glOBStFR/view?usp=sharing",
+    // video: game3,
+  },
+  {
+    title: "Game Escape the Forest",
+    image: project4,
+    description:
+      "This is a game about a man trapped in a forest. In the forest, there is a monster, and the man must explore the area to find batteries to unlock a gate and escape.",
+    technologies: ["Unreal Engine", "Blueprints", "State Machine"],
+    link: "https://drive.google.com/file/d/1ySd3wtZKg7UdNy8qdmKcg1j7YwuSFEDg/view?usp=sharing",
+    // video: game4,
+  },
+];
+
+export const CONTACT = {
+  address: "140 dương thị mười Q12 TPHCM",
+  phoneNo: "0345524769",
+  email: "tranhoa29022@gmail.com",
+};
